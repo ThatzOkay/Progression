96c278f44cc681e000b5af230d8eda028bee79f1
ThatzOkay <10085767@edu.regiocollege.nl>
2020-10-30 15:56:16 +0100

    Updated .net 4.8 Framework

--- Progression.Tests/Program.cs
+++ Progression.Tests/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-
-namespace Progression.Tests
+﻿namespace Progression.Tests
 {
     class Program
     {
